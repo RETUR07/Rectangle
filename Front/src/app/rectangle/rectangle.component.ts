ef3a85b87f94004a6375d5eb305067b070775d13
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-03-25 16:41:00 +0300

    fixed resize

--- Front/src/app/rectangle/rectangle.component.ts
+++ Front/src/app/rectangle/rectangle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Rectangle } from 'src/app/models/rectangle.model';
 import { RectangleHttpService } from 'src/app/services/rectangleHttpService';
 
@@ -30,12 +30,7 @@ export class RectangleComponent {
     );
   }
 
-  onMouseLeave(event: any)
-  {
-    if(this.IsEdited)
-      this.onMouseUp(event);
-  }
-
+  @HostListener('document:mousedown', ['$event'])
   onMouseDown(event: any)
   {
     this.IsEdited = true;
@@ -53,6 +48,7 @@ export class RectangleComponent {
     }
   }
 
+  @HostListener('document:mouseup', ['$event'])
   onMouseUp(event: any)
   {
     this.resizePoint = 0;
@@ -61,29 +57,24 @@ export class RectangleComponent {
     this.http.SaveRectangle(this.rectangle).subscribe();
   }
 
+  @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: any)
   {
-    const invisible_border = 10;
-    //check border
-    if(event.clientX + invisible_border > this.canvasX || 
-       event.clientX - invisible_border < 0 ||
-       event.clientY + invisible_border > this.canvasY || 
-       event.clientY - invisible_border < 0
-       )
-       {
-         return;
-       }
+    const invisible_border = 20;
 
     //check create or resize
     if (this.IsEdited)
       if (!this.rectangleCreated){
-        this.rectangle.rectX2 = event.clientX;
-        this.rectangle.rectY2 = event.clientY;
+        if (event.clientX + invisible_border < this.canvasX && event.clientX - invisible_border > 0)
+          this.rectangle.rectX2 = event.clientX;
+        if (event.clientY + invisible_border < this.canvasY && event.clientY - invisible_border > 0)
+          this.rectangle.rectY2 = event.clientY;
       }
       else{
         const speed = 1;
         const xChange = (this.xResizeStart - event.clientX)/speed;
         const yChange = (this.yResizeStart - event.clientY)/speed;
+
         //check resize point
         if(Math.abs(this.rectangle.rectX2 - event.clientX) < 20 && Math.abs(this.rectangle.rectY2 - event.clientY) < 20)
         {
@@ -112,20 +103,59 @@ export class RectangleComponent {
         switch(this.resizePoint)
         {
           case 1:
-            this.rectangle.rectX2 = Math.abs(this.rectangle.rectX2 - xChange);
-            this.rectangle.rectY2 = Math.abs(this.rectangle.rectY2 - yChange);
+            if(
+              this.rectangle.rectX2 - xChange + invisible_border < this.canvasX && 
+              this.rectangle.rectX2 - xChange - invisible_border > 0
+            ) 
+            this.rectangle.rectX2 = this.rectangle.rectX2 - xChange;
+
+            if(
+              this.rectangle.rectY2 - yChange + invisible_border < this.canvasY && 
+              this.rectangle.rectY2 - yChange - invisible_border > 0
+            )
+            this.rectangle.rectY2 = this.rectangle.rectY2 - yChange;
+
             break;
           case 2:
-            this.rectangle.rectX1 = Math.abs(this.rectangle.rectX1 - xChange);
-            this.rectangle.rectY1 = Math.abs(this.rectangle.rectY1 - yChange);
+            if(
+              this.rectangle.rectX1 - xChange + invisible_border < this.canvasX && 
+              this.rectangle.rectX1 - xChange - invisible_border > 0
+            )
+            this.rectangle.rectX1 = this.rectangle.rectX1 - xChange;
+
+            if(
+              this.rectangle.rectY1 - yChange + invisible_border < this.canvasY && 
+              this.rectangle.rectY1 - yChange - invisible_border > 0
+            )
+            this.rectangle.rectY1 = this.rectangle.rectY1 - yChange;
+
             break;
           case 3:
-            this.rectangle.rectX1 = Math.abs(this.rectangle.rectX1 - xChange);
-            this.rectangle.rectY2 = Math.abs(this.rectangle.rectY2 - yChange);
+            if(
+              this.rectangle.rectX1 - xChange + invisible_border < this.canvasX && 
+              this.rectangle.rectX1 - xChange - invisible_border > 0
+            )
+            this.rectangle.rectX1 = this.rectangle.rectX1 - xChange;
+
+            if(
+              this.rectangle.rectY2 - yChange + invisible_border < this.canvasY && 
+              this.rectangle.rectY2 - yChange - invisible_border > 0
+            )
+            this.rectangle.rectY2 = this.rectangle.rectY2 - yChange;
+
             break;
           case 4:
-            this.rectangle.rectX2 = Math.abs(this.rectangle.rectX2 - xChange);
-            this.rectangle.rectY1 = Math.abs(this.rectangle.rectY1 - yChange);
+            if(
+              this.rectangle.rectX2 - xChange + invisible_border < this.canvasX && 
+              this.rectangle.rectX2 - xChange - invisible_border > 0
+            ) 
+            this.rectangle.rectX2 = this.rectangle.rectX2 - xChange;
+
+            if(
+              this.rectangle.rectY1 - yChange + invisible_border < this.canvasY && 
+              this.rectangle.rectY1 - yChange - invisible_border > 0
+            ) 
+            this.rectangle.rectY1 = this.rectangle.rectY1 - yChange;
             break;
         }
 
