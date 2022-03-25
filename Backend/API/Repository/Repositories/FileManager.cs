1a0c4b3286350a4292291069414e4da982747366
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-03-25 14:28:20 +0300

    added file manager

---
+++ Backend/API/Repository/Repositories/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using Repository.Configuration;
+using Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class FileManager : IFileManager
+    {
+        private readonly object _fileAccess = new object();
+
+        public async Task<string> ReadFileAsync()
+        {
+            string str;
+            lock (_fileAccess)
+            {
+                using (StreamReader r = new StreamReader(JSONConfig.FileName))
+                {
+                    str = r.ReadToEnd();
+                };
+            }
+            return str;
+        }
+
+        public async Task WriteFileAsync(string str)
+        {
+            lock (_fileAccess)
+            {
+                using (FileStream fileStream = File.Open(JSONConfig.FileName, FileMode.Create))
+                {
+                    using (StreamWriter w = new StreamWriter(fileStream))
+                    {
+                        w.WriteLine(str);
+                        w.Flush();
+                    };
+                };
+            }
+        }
+    }
+}
